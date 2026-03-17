989e919439b618c332de35ba12a63bf4390d85a3
timofeiemilian9000-alt <timofeiemilian9000@gmail.com>
2026-03-17 11:33:42 +0100

    cleaned

--- 2FA_Verification/index.py
+++ 2FA_Verification/index.py
@@ -5,7 +5,12 @@ attempt = "Safety1st!"
 code_sent = 836027  
 code_entered = 836027
 
-if locked == False and attempt == password and code_entered == code_sent:
+unlocked = locked == False
+attempt_ok = attempt == password
+code_ok = code_sent == code_entered
+login_success = unlocked and attempt_ok and code_ok
+
+if login_success:
     print("Login successful!")
 else: 
     print("Login failed!")
